3ed784f77df60afc49230e4c488c505d6f4600f9
Hernán Castro <hernandc96@gmail.com>
2026-08-12 14:05:51 -0300

    Creación de la clase modelo Product con constructor y getters

---
+++ src/com/tienda/model/Product.java
@@ -0,0 +1,34 @@
+package com.tienda.model;
+
+public class Product {
+
+    // Atributos
+    private String title;
+    private int year;
+    private double price;
+    private int stock;
+
+    // Constructores
+    public Product(String title, int year, double price, int stock){
+        this.title = title;
+        this.year = year;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    // Getters
+    public String getTitle() {
+        return title;
+    }
+    public int getYear() {
+        return year;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public int getStock() {
+        return stock;
+    }
+
+
+}
