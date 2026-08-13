5e7c384d9c17644f0f6081fbb8bfd6a9fc13b23e
Hernán Castro <hernandc96@gmail.com>
2026-08-12 22:02:12 -0300

    Creación de subclase VideoGame con atributos, constructor, llamada a super() y getters

---
+++ src/com/tienda/model/VideoGame.java
@@ -0,0 +1,29 @@
+package com.tienda.model;
+
+public class VideoGame extends Product {
+
+    // Atributos
+    private String consoleGame;
+    private boolean isMultiplayer;
+    private String gameGenre;
+    private String packagingType;
+    private String region;
+
+    // Constructores
+    public VideoGame (String consoleGame, boolean isMultiplayer, String gameGenre, String packagingType, String region,String title, int year, double price, int stock) {
+    super (title, year, price, stock);
+    this.consoleGame = consoleGame;
+    this.isMultiplayer = isMultiplayer;
+    this.gameGenre = gameGenre;
+    this.packagingType = packagingType;
+    this. region = region;
+    }
+
+    // Getters
+    public String getConsoleGame(){return consoleGame;}
+    public boolean isMultiplayer(){return isMultiplayer;}
+    public String getGameGenre(){return gameGenre;}
+    public String getPackagingType(){return packagingType;}
+    public String getRegion(){return region;}
+
+}
