46603cc8fa30772e3b4478308b6048d685b77ab7
Hernán Castro <hernandc96@gmail.com>
2026-08-20 10:50:55 -0300

    Proyecto 1 finalizado

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import com.sun.source.doctree.SinceTree;
 import com.tienda.model.Product;
 import com.tienda.model.RetroConsole;
 import com.tienda.model.VideoGame;
@@ -92,42 +91,6 @@ public class Main {
 
             }
         }
-
         lector.close();
-/*
-        // Instanciar objetos -Hardcoding test-
-        // Consolas
-        Product ps1 = new RetroConsole("PlayStation 1", 1994, 150000, 15, false, "CD", 5);
-        Product ps2 = new RetroConsole("PlayStation 2", 2000, 400000, 30, false, "DVD", 6);
-        Product gba = new RetroConsole("Game Boy Advance", 2001, 100000, 30, true, "Cartucho", 6);
-        Product psp = new RetroConsole("PlayStation Portable", 2004, 200000, 25, true, "UMD", 7);
-        Product snes = new RetroConsole("Super Nintendo", 1990, 300000, 10, false, "Cartucho", 4);
-
-        // Videojuegos
-        Product re1 = new VideoGame("Resident Evil", 1996, "PlayStation 1", false, "Survival Horror", "NTSC", 20000, 50);
-        Product marioWorld = new VideoGame("Super Mario World", 1990, "Super Nintendo", true, "Plataformas", "NTSC", 15000, 45);
-        Product pkmEsmeralda = new VideoGame("Pókemon Esmeralda", 2004, "Game Boy Advance", true, "RPG", "NTSC", 60000, 60);
-        Product gtaSA = new VideoGame("Grand Theft Auto: San Andreas", 2004, "PlayStation 2", true, "Mundo Abierto", "NTSC", 80000, 120);
-        Product gow = new VideoGame("God of War", 2005, "PlayStation 2", false, "Hack and Slash", "NTSC", 40000, 100);
-        Product tekken3 = new VideoGame("Tekken 3", 1998, "PlayStation 1", true, "Lucha", "NTSC", 30000, 60);
-        Product gtaVCS = new VideoGame("Grand Theft Auto: Vice City Stories", 2006, "PlayStation Portable", false, "Mundo Abierto", "NTSC", 50000, 90);
-
-        // Inventario
-        Inventory inventory = new Inventory();
-        inventory.addProduct(ps1);
-        inventory.addProduct(ps2);
-        inventory.addProduct(psp);
-        inventory.addProduct(gba);
-        inventory.addProduct(snes);
-        inventory.addProduct(re1);
-        inventory.addProduct(marioWorld);
-        inventory.addProduct(gtaSA);
-        inventory.addProduct(gtaVCS);
-        inventory.addProduct(gow);
-        inventory.addProduct(tekken3);
-
-        // Prueba de metodos de Inventory
-        inventory.showCatalog();
-*/
     }
 }
